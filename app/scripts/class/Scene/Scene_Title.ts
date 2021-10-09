5cc8102fa311fb768667701276b52877b297d749
sinnkami <sinnkami@windows10>
2021-10-09 19:39:16 +0900

    仮データをタイトル画面から遷移する際に読み込むように実装

--- app/scripts/class/Scene/Scene_Title.ts
+++ app/scripts/class/Scene/Scene_Title.ts
@@ -1,6 +1,9 @@
+import { IStoreCharacter } from "../../definitions/class/Store/IStoreCharacter";
 import { CommonConstruct, KeyCode } from "../Construct/CommonConstruct";
+import DataManager from "../Manager/DataManager";
 import EventManager, { EventCode } from "../Manager/EventManager";
 import GameManager from "../Manager/GameManager";
+import StoreManager from "../Manager/StoreManager";
 import { Sprite_Background } from "../Sprite/Sprite_Background";
 import Scene_Base from "./Scene_Base";
 
@@ -61,6 +64,7 @@ export default class Scene_Title extends Scene_Base {
 	/**
 	 * 決定キーに関するプロセスを設定
 	 * TODO: そのうちタイトルに関する処理に代わると思う
+	 * TODO: 初めから・続きから・終了が必要そう
 	 * @returns
 	 */
 	private async setProcessInputSelect(): Promise<void> {
@@ -68,6 +72,8 @@ export default class Scene_Title extends Scene_Base {
 			name: ProcessName.InputSelect,
 			process: async (time: number) => {
 				if (GameManager.input.isPushedKey(KeyCode.Select)) {
+					// TODO: 強制初めから処理
+					await this.executeInitStart();
 					const key = GameManager.input.getKey(KeyCode.Select);
 
 					// ロビー画面表示イベントを取得
@@ -76,5 +82,36 @@ export default class Scene_Title extends Scene_Base {
 				}
 			},
 		});
+	}
+
+	/**
+	 * 初めから処理
+	 * TODO: メソッドの箇所
+	 */
+	private async executeInitStart(): Promise<void> {
+		// TODO: 現在はテスト用の値まみれ
+		// とりあえずキャラ追加
+		const reimu = DataManager.character.get("0001") as IStoreCharacter;
+		const sakuya = DataManager.character.get("0009") as IStoreCharacter;
+		const youmu = DataManager.character.get("0017") as IStoreCharacter;
+		StoreManager.character.add(reimu, sakuya, youmu);
+
+		console.log(reimu, sakuya, youmu);
+
+		// パーティーを勝手に設定
+		StoreManager.party.add(
+			{
+				characterId: reimu.characterId,
+				order: 3,
+			},
+			{
+				characterId: sakuya.characterId,
+				order: 2,
+			},
+			{
+				characterId: youmu.characterId,
+				order: 1,
+			}
+		);
 	}
 }
