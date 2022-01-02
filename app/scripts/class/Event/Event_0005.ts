084d72f65ca39987c544b4de206624fa43ebd264
sinnkami <sinnkami@windows10>
2022-01-02 19:48:44 +0900

    メッセージフレーム用の画像から作成するように変更

--- app/scripts/class/Event/Event_0005.ts
+++ app/scripts/class/Event/Event_0005.ts
@@ -24,7 +24,7 @@ export class Event_0005 extends Event_Base {
 
 		loadResources.push({
 			name: "menu-background",
-			path: "assets/images/window/menu/normal.png",
+			path: "assets/images/window/menu/test.png",
 		});
 
 		for (const partyInfo of GameManager.party.getMenberList()) {
